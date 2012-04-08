b91916c64bffe0c941ee81cfb0618f1e8c25d57c
Frank Kleine <mikey@stubbles.net>
2012-04-08 15:41:16 +0200

    add interceptor interfaces

---
+++ src/main/php/net/stubbles/webapp/interceptor/PostInterceptor.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of stubbles.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package  net\stubbles\webapp
+ */
+namespace net\stubbles\webapp\interceptor;
+use net\stubbles\input\web\WebRequest;
+use net\stubbles\lang\Object;
+use net\stubbles\webapp\response\Response;
+/**
+ * Interface for post interceptors.
+ *
+ * Postinterceptors are called after all data processing is done. They can change
+ * the response or add additional data to the response.
+ */
+interface PostInterceptor extends Object
+{
+    /**
+     * does the postprocessing stuff
+     *
+     * @param  WebRequest  $request   access to request data
+     * @param  Response    $response  access to response data
+     */
+    public function postProcess(WebRequest $request, Response $response);
+}
+?>
---
+++ src/main/php/net/stubbles/webapp/interceptor/PreInterceptor.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of stubbles.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package  net\stubbles\webapp
+ */
+namespace net\stubbles\webapp\interceptor;
+use net\stubbles\input\web\WebRequest;
+use net\stubbles\lang\Object;
+use net\stubbles\webapp\response\Response;
+/**
+ * interface for pre interceptors.
+ *
+ * Preinterceptors are called after all initializations have been done and
+ * before processing of data starts.
+ */
+interface PreInterceptor extends Object
+{
+    /**
+     * does the preprocessing stuff
+     *
+     * @param  WebRequest  $request   access to request data
+     * @param  Response    $response  access to response data
+     */
+    public function preProcess(WebRequest $request, Response $response);
+}
+?>
