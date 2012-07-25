b3d602ec3f08ffcbf897e6ec02e054caf31a9466
mikey179 <mikey@stubbles.net>
2012-07-25 17:31:55 +0200

    use new getProtocolVersion() method, refactor

--- src/main/php/net/stubbles/webapp/response/ResponseCreator.php
+++ src/main/php/net/stubbles/webapp/response/ResponseCreator.php
@@ -11,29 +11,46 @@ namespace net\stubbles\webapp\response;
 use net\stubbles\lang\BaseObject;
 /**
  * Creates response with correct HTTP version based on a given HTTP version.
- *
- * If the HTTP version is not supported it will create a response with version
- * HTTP/1.1 and status code set to 500, additionally cancelling the request.
  */
 class ResponseCreator extends BaseObject
 {
     /**
      * creates response instance
      *
+     * If the HTTP version is not supported it will create a response with version
+     * HTTP/1.1 and status code set to 500.
+     *
      * @param   string  $httpProtocol   http protocol to create response for
      * @param   string  $responseClass  concrete response class to create
      * @return  Response
      */
-    public static function create($httpProtocol, $responseClass = 'net\stubbles\webapp\response\WebResponse')
+    public static function createForProtocol($httpProtocol, $responseClass = 'net\stubbles\webapp\response\WebResponse')
     {
-        $minor      = null;
-        $scanResult = sscanf($httpProtocol, 'HTTP/%*[1].%[01]', $minor);
-        if (2 != $scanResult) {
-            $response = new $responseClass();
-            $response->setStatusCode(505);
-            $response->write('Unsupported HTTP protocol version, expected HTTP/1.0 or HTTP/1.1');
+        $minor = null;
+        if (1 != sscanf($httpProtocol, 'HTTP/1.%[01]', $minor)) {
+            $response = self::createForVersion(null, $responseClass);
         } else {
-            $response = new $responseClass('1.' . ((int) $minor));
+            $response = self::createForVersion('1.' . $minor, $responseClass);
+        }
+
+        return $response;
+    }
+
+    /**
+     * creates response instance
+     *
+     * @param   string  $httpVersion   http protocol to create response for
+     * @param   string  $responseClass  concrete response class to create
+     * @return  Response
+     */
+    public static function createForVersion($httpVersion, $responseClass = 'net\stubbles\webapp\response\WebResponse')
+    {
+        if (empty($httpVersion)) {
+            $response = new $responseClass('1.1');
+            $response->setStatusCode(505)
+                     ->write('Unsupported HTTP protocol version, expected HTTP/1.0 or HTTP/1.1');
+        } else {
+            $response = new $responseClass($httpVersion);
         }
 
         return $response;
